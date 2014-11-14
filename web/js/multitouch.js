9d32c04d917eebd18e003e7b7aa9331103656310
jejeLaVedette <PolytechRICM@192.168.1.81>
2014-11-14 11:57:06 +0100

    deplacement bugger

--- web/js/multitouch.js
+++ web/js/multitouch.js
@@ -7,13 +7,16 @@ define    ( [
     //http://stackoverflow.com/questions/4850821/svg-coordinates-with-transform-matrix
 	console.log("---> Start");
     svg= document.querySelector("svg");
+    
+    var StockPointElem = {};
+    var ElementDeTouchId = {};
 
     var E = document.querySelectorAll("svg .deplacable");
     for(var i=0;i<E.length;i++){
         E.item(i).addEventListener("touchstart", function (event){
             console.log("touchStart event "+this.id);
             event.preventDefault();
-            onStart(event);
+            onStart(event,this);
 
         },false);
         E.item(i).addEventListener("touchmove", function (event){
@@ -25,25 +28,29 @@ define    ( [
 
 
     function onMove(e){
-        var matrice = svg.getScreenCTM().inverse()
-        //déplacer le dessin ici
-        var pt = svg.createSVGPoint();
-        pt.x = evt.clientX;
-        pt.y = evt.clientY;
-        var globalPoint = pt.matrixTransform(matrice);
+        var L = e.changedTouches;
+        for(var i = 0; i < L.length; i++){
+            var pts = L.item(i);
+            var elem = StockPointElem[L.item(i).identifier].newPoint;
+            var matrice = elem.getCTM().inverse();
+            //déplacer le dessin ici
+            var pt = svg.createSVGPoint();
+            pt.x = pts.clientX;
+            pt.y = pts.clientY;
+            var globalPoint = pt.matrixTransform(matrice);
+            var newPoint = StockPointElem[L.item(i).identifier].coordonnees;
 
-        this.setAttribute('transform','translate('+globalPoint.x+','+globalPoint.y+')');
+            elem.setAttribute('transform','translate('+(globalPoint.x-newPoint.x)+','+(globalPoint.y-newPoint.y)+')');
+        }
     }
 
 
-    function onStart(e){
-        /* enregistrement, lors du touchstart, la correspondance entre l'identifiant du touch et l'élement touché, ainsi que des infos complémentaires...
-        */
+    function onStart(e,obj){
+        /* enregistrement, lors du touchstart, la correspondance entre l'identifiant du touch et l'élement touché, ainsi que des infos
+        complémentaires... */
         /*var ElementDeTouchId = {};
         ElementDeTouchId[touch.identifier]=ElementSVG;
-        /* = { Element : ElementSVG
-
-        */
+        /* = { Element : ElementSVG */
         var L = e.changedTouches;
 		for(var i = 0; i < L.length; i++){
 			var touch = L.item(i);
@@ -51,8 +58,8 @@ define    ( [
 			var newP = svg.createSVGPoint();
 			newP.x = touch.pageX;
 			newP.y = touch.pageY;
-			newP = newP.matrixTransform(elem.getCTM().inverse());
-			StockPointElem[id] = {coordonnees : newP, elemAsso : elem};
+			newP = newP.matrixTransform(obj.getCTM().inverse());
+			StockPointElem[id] = {coordonnees : newP, newPoint : obj};
 		}
     }
 });
