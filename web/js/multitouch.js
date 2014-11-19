762501e184c4a93628fe39d08d6b577c362c32da
jejeLaVedette <PolytechRICM@192.168.1.81>
2014-11-19 10:40:16 +0100

    les objets sont maintenants deplacable par le clique : love

--- web/js/multitouch.js
+++ web/js/multitouch.js
@@ -3,19 +3,19 @@ define    ( [
         , function(ALX_magictouch) {
     // Coder ici les interactions de translation et de transformation affine
 
-
-    //http://stackoverflow.com/questions/4850821/svg-coordinates-with-transform-matrix
+    /*Code realiser avec l'aide du binome FREBY-LABAT, il y aura donc des ressemblances*/
 	console.log("---> Start");
+    
+    //balise svg du html
     svg= document.querySelector("svg");
     
-    var StockPointElem = {};
-    var ElementDeTouchId = {};
-
+    var StockElem = {};
+    
+    //objet html deplacable
     var E = document.querySelectorAll("svg .deplacable");
     for(var i=0;i<E.length;i++){
         E.item(i).addEventListener("touchstart", function (event){
             console.log("touchStart event "+this.id);
-            event.preventDefault();
             onStart(event,this);
 
         },false);
@@ -27,30 +27,9 @@ define    ( [
     }
 
 
-    function onMove(e){
-        var L = e.changedTouches;
-        for(var i = 0; i < L.length; i++){
-            var pts = L.item(i);
-            var elem = StockPointElem[L.item(i).identifier].newPoint;
-            var matrice = elem.getCTM().inverse();
-            //déplacer le dessin ici
-            var pt = svg.createSVGPoint();
-            pt.x = pts.clientX;
-            pt.y = pts.clientY;
-            var globalPoint = pt.matrixTransform(matrice);
-            var newPoint = StockPointElem[L.item(i).identifier].coordonnees;
-
-            elem.setAttribute('transform','translate('+(globalPoint.x-newPoint.x)+','+(globalPoint.y-newPoint.y)+')');
-        }
-    }
-
-
     function onStart(e,obj){
         /* enregistrement, lors du touchstart, la correspondance entre l'identifiant du touch et l'élement touché, ainsi que des infos
         complémentaires... */
-        /*var ElementDeTouchId = {};
-        ElementDeTouchId[touch.identifier]=ElementSVG;
-        /* = { Element : ElementSVG */
         var L = e.changedTouches;
 		for(var i = 0; i < L.length; i++){
 			var touch = L.item(i);
@@ -59,7 +38,41 @@ define    ( [
 			newP.x = touch.pageX;
 			newP.y = touch.pageY;
 			newP = newP.matrixTransform(obj.getCTM().inverse());
-			StockPointElem[id] = {coordonnees : newP, newPoint : obj};
+            var MyMatrice =obj.parentElement.getCTM().inverse().multiply(obj.getCTM());
+
+			StockElem[id] = {coordonnees : newP, obj : obj, parent : obj.parentElement, matrice : MyMatrice};
 		}
     }
+    
+    function onMove(e){
+        var L = e.changedTouches;
+        for(var i = 0; i < L.length; i++){
+            var pts = L.item(i);
+            //verifie que le point n'est pas null
+            if(typeof StockElem[pts.identifier] === "undefined") {continue;}
+            
+            var elem = StockElem[pts.identifier].obj;
+            var coordonnees = StockElem[pts.identifier].coordonnees;
+            
+            //déplacer le dessin
+            var pt = svg.createSVGPoint();
+            pt.x = pts.pageX;
+            pt.y = pts.pageY;
+            // Coordonnées du pointeur par rapport au parent de l'élément à déplacer
+            var parent = StockElem[pts.identifier].parent;
+            coordonneesRelativeAParent = pt.matrixTransform(parent.getCTM().inverse());
+
+            // Résoud l'équation
+            var M = StockElem[pts.identifier].matrice;
+            var e = coordonneesRelativeAParent.x - M.a*coordonnees.x - M.c *coordonnees.y;
+            var f = coordonneesRelativeAParent.y - M.b*coordonnees.x - M.d *coordonnees.y;
+            // Affecter la matrice de transformtion
+            elem.setAttribute('transform', 'matrix('+M.a+','+M.b+','+M.c+','+M.d+','+e+','+f+')' );
+               
+        }
+    
+    }
+    
+    
+    
 });
